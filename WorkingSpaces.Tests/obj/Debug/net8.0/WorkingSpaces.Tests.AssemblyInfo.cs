55a351e7f823cb26d7c32651006105fe8ce76d9b
ssherliann <popova_anna@knu.ua>
2025-10-02 08:23:32 +0300

    add styles

--- WorkingSpaces.Tests/obj/Debug/net8.0/WorkingSpaces.Tests.AssemblyInfo.cs
+++ WorkingSpaces.Tests/obj/Debug/net8.0/WorkingSpaces.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkingSpaces.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0b8b134c0041b3f67f416e38b75c0f392edede2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkingSpaces.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkingSpaces.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
